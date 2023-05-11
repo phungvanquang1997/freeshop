b3263e84b20c671233c3ecc3a4db30490f9b01bd
quangpv <phungvanquang1997@gmail.com>
2023-05-11 22:32:56 +0700

    t2

---
+++ public/js/admin/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by JFog on 3/21/2016.
+ */
+
+$(document).ready(function($) {
+
+
+
+});
+
---
+++ public/test.php
@@ -0,0 +1,27 @@
+<style>
+  .flag_cell {
+    width: 100px;
+    height: 100px;
+  }
+  .flag_red {
+    background-color: #d90000;
+  }
+  .flag_yellow {
+    background-color: #ffff00;
+  }
+  .flag_green {
+    background-color: #00b300;
+  }
+</style>
+
+<table>
+  <tr>
+    <td class="flag_cell flag_red"></td>
+  </tr>
+  <tr>
+    <td class="flag_cell flag_yellow"></td>
+  </tr>
+  <tr>
+    <td class="flag_cell flag_green"></td>
+  </tr>
+</table>
