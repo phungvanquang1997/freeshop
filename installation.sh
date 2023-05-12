2332508807149d5e96b69b81f344ca5078ddc265
quangpv <phungvanquang1997@gmail.com>
2023-05-12 20:39:26 +0700

    fix missing files

--- installation.sh
+++ installation.sh
@@ -1,2 +1,3 @@
  cp -r ./files_lar5_to_lar10/Pagination/* vendor/laravel/framework/src/Illuminate/Pagination
  cp -r ./files_lar5_to_lar10/ContractsPagination/* vendor/laravel/framework/src/Illuminate/Contracts/Pagination
+ cp ./files_lar5_to_lar10/Foundation/Auth/AuthenticatesAndRegistersUsers.php vendor/laravel/framework/src/Illuminate/Foundation/Auth
