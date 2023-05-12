c9e64553e64f1e048a5c58da1bda7eef5bc66a9d
quangpv <phungvanquang1997@gmail.com>
2023-05-12 17:47:59 +0700

    add lar5's files

---
+++ installation.sh
@@ -0,0 +1,2 @@
+ cp -r ./files_lar5_to_lar10/Pagination/* vendor/laravel/framework/src/Illuminate/Pagination
+ cp -r ./files_lar5_to_lar10/ContractsPagination/* vendor/laravel/framework/src/Illuminate/Contracts/Pagination
